0674eb9b5064cc5fd20dd9376f95dd2c185534d4
Matar Davis <matar@nx6.priv>
2014-09-05 10:25:10 -0700

    save alpha to csv file, added cv to get mean prediction score

--- HG_regression.py
+++ HG_regression.py
@@ -15,14 +15,15 @@ def HG_regression_allelecs_SGE(DATASET):
     SJdir = '/home/knight/matar/MATLAB/DATA/Avgusta'
     subj, task = DATASET.split('_')
     
-    betas, predscores, elecs, features = HG_regression_allelecs(subj, task)
+    betas, predscores, elecs, features, all_alphas = HG_regression_allelecs(subj, task)
 
     filename = os.path.join(SJdir, 'PCA', 'Stats', 'Regression', '_'.join([subj, task]))
 
-    pickle.dump({'betas': betas, 'predscores':predscores, 'elecs': elecs, 'features': features}, open(filename + '.p', 'wb'))
+    pickle.dump({'betas': betas, 'predscores':predscores, 'elecs': elecs, 'features': features, 'alphas':all_alphas}, open(filename + '.p', 'wb'))
     
-    data_array =  np.hstack((np.asarray(betas), np.reshape(predscores, (len(predscores), 1))))
+    data_array =  np.hstack((np.asarray(betas), np.reshape(predscores, (len(predscores), 1)), np.reshape(all_alphas, (len(all_alphas), 1))))
     features.append('pred_score')
+    features.append('alpha')
 
     df = pd.DataFrame(data_array, columns = features, index = elecs)
     df.to_csv(filename + '_betas.csv')
@@ -44,7 +45,8 @@ def HG_regression_allelecs(subj, task):
     # load data
     filename = os.path.join(SJdir, 'PCA', 'ShadePlots_hclust', 'elecs', 'significance_windows', 'data', ''.join([subj, '_', task, '.p']))
     data_dict = pickle.load(open(filename, 'rb'))
-    betas, predscores = [[] for i in range(2)]
+   
+    all_alphas, betas, predscores = [[] for i in range(3)]
 
     #set parameters
     #features = ['maxes', 'means', 'stds', 'sums', 'lats_pro']
@@ -64,34 +66,44 @@ def HG_regression_allelecs(subj, task):
         X = X[~mask]
         mask = np.isnan(Y)
         Y = Y[~mask]
-
-        #define training and test set
-        X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, Y, test_size = 0.2)
         
-        #normalize
-        X_train = scale(X_train.astype(float))
-        X_test = scale(X_test.astype(float))
-        y_train = scale(y_train.astype(float))
-        y_test = scale(y_test.astype(float))
+        #external cv for prediction score stability
+        nfolds = 100
+        scores, coefs, alphas_cv = [[] for x in range(3)]
+        for c in range(nfolds):
+            #define training and test set
+            X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, Y, test_size = 0.2)
+
+            #normalize
+            X_train = scale(X_train.astype(float))
+            X_test = scale(X_test.astype(float))
+            y_train = scale(y_train.astype(float))
+            y_test = scale(y_test.astype(float))
+
+            #fit ridge regression with cross validation on training set
+            alphas, models, scores, coefs = fit_model(X_train, y_train)
+
+            #fit final model using mean hyperparam on all training data
+            alpha = np.median(alphas) #alphas are not normally distributed, more stable if take median
+            model = Ridge(solver = 'lsqr', alpha = alpha, normalize = False, fit_intercept = False)
+            model.fit(X_train, y_train)
+
+            #calculate prediction accuracy
+            scores.append(np.corrcoef(model.predict(X_test), y_test)[0,1])
+            coefs.append(model.coef_)
+            alphas_cv.append(alpha)
         
-        #fit ridge regression with cross validation on training set
-        alphas, models, scores, coefs = fit_model(X_train, y_train)
+        print (elec, np.median(scores))
+        sys.stdout.flush()
 
-        #fit final model using mean hyperparam on all training data
-        model = Ridge(solver = 'lsqr', alpha = np.mean(alphas), normalize = False, fit_intercept = False)
-        model.fit(X_train, y_train)
-
-        #calculate prediction accuracy
-        score = np.corrcoef(model.predict(X_test), y_test)[0,1]
-        coefs = model.coef_
-
-        betas.append(coefs)
-        predscores.append(score)
-
-    return betas, predscores, elecs, features
+        betas.append(np.median(coefs, axis = 0))
+        predscores.append(np.median(scores))
+        all_alphas.append(np.median(alphas_cv))
+        
+    return betas, predscores, elecs, features, all_alphas
 
 
-def fit_model(X_train, y_train, cv = 100, test_size = .25):
+def fit_model(X_train, y_train, cv = 100, test_size = .2):
     """
     takes in an array of features x observations and an array of predictors
     runs ridge with cv, grid search on alphas
