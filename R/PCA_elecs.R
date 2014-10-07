a8ab1208225462f47996b55ade638f964507dcb4
Matar Davis <matar@nx6.priv>
2014-10-07 15:28:17 -0700

    implemented PCA using the new outlier rejected trials (less strict than before)

--- R/PCA_elecs.R
+++ R/PCA_elecs.R
@@ -1,42 +1,38 @@
 setwd('/home/knight/matar/PYTHON/ECOGpy/')
-library(psych)
+#library(psych)
 library(proxy)
-source("R/EFA_Comparison.R")
+#source("R/EFA_Comparison.R")
 library(reshape2)
-library(ggplot2)
-library(cluster)
+#library(ggplot2)
+#library(cluster)
+#library(lattice)
 
-param = 'maxes' #or lats_pro or means
+params = c('maxes_rel', 'lats_pro','medians','stds')
 
-DATADIR = '/home/knight/matar/MATLAB/DATA/Avgusta/PCA/ShadePlots_hclust/elecs/significance_windows/csv_files/' #csv files with values for each subj/task 
-saveDir = paste('/home/knight/matar/MATLAB/DATA/Avgusta/PCA/Stats/PCA_', param, '/', sep = "")
+DATADIR = '/home/knight/matar/MATLAB/DATA/Avgusta/PCA/Stats/outliers/for_PCA/' #csv files with values for each subj/task with outlier rejection for PCA (all elecs have same number of trials, each feature has different number of trials)
 
 d = read.table('test.subjects',sep = '_')
 
-for (i in 13:nrow(d) ) {
-#for (i in c(36,37,38,39,40,41)) {  
-  subj = d$V1[i]
-  task = d$V2[i]
-  
-  data = read.csv(paste(DATADIR, subj, '_', task, '_', param, '.csv', sep = ""), header = T, stringsAsFactors = F)
-  data = data.matrix(data)
-  
-  #drop rows with NA values
-  #data = data[complete.cases(data), ]
+for (i in 1:nrow(d) ) {
+  for (p in params) {
+    saveDir = paste('/home/knight/matar/MATLAB/DATA/Avgusta/PCA/Stats/PCA_', p, '/', sep = "")
     
-  efa = EFA.Comp.Data(Data=data, F.Max=10, Graph=T) #calculate number of components
-  filename = paste(saveDir, subj, "_", task, '_efa.png',sep = "")
-  dev.copy(png,filename)
-  dev.off()
-           
-  fit = principal(data, nfactors = efa, rotate='none') #calculate PCA without rotation
+    subj = d$V1[i]
+    task = d$V2[i]
+        
+    data = read.csv(paste(DATADIR, subj, '_', task, '_', p, '.csv', sep = ""), header = T, stringsAsFactors = F)
+    data = data.matrix(data)
+    
+    #implement kaiser rule
+    fit = prcomp(data, scale=TRUE, retx = FALSE) #uses correlation matrix, no rotation
+    eigvalues = fit$sdev^2 #(sdev aka signular values, works bc sum of eigvalues = sum total of the variance)
+    num_pcs = sum(eigvalues>1)
+    
+    #write loadings csv
+    filename = paste(saveDir, subj, '_', task, '_loadings.csv',sep = "")
+    write.table(fit$rotation[,1:num_pcs],file = filename, sep =",", row.names=TRUE, col.names = NA)
+    filename = paste(saveDir, subj, '_', task, '_summary.csv',sep = "")
+    write.table(summary(fit)$importance[,1:num_pcs],file = filename, sep =",", row.names = TRUE, col.names = NA)
   
-  #write loadings csv
-  df_loadings = cbind(data.frame(unclass(fit$loadings)), data.frame(fit$communality))
-  df_SSloadings = data.frame(fit$values[1:efa])
-  filename = paste(saveDir, subj, '_', task, '_loadings.csv',sep = "")
-  write.table(df_loadings,file = filename, sep =",", row.names=TRUE, col.names = NA)
-  filename = paste(saveDir, subj, '_', task, '_SSloadings.csv',sep = "")
-  write.table(df_SSloadings,file = filename, sep =",", row.names=FALSE)
-
+  }
 }
